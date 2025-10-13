a88390279661232733e27f7d8655f7ab036306c3
David Carlos <Davidxsantos@outlook.com>
2025-10-13 19:43:03 -0300

    EstruturaEscolhaCaso1

---
+++ EXE.estruturaEscolhaCaso1C#/EXE.estruturaEscolhaCaso1C#/Program.cs
@@ -0,0 +1,43 @@
+﻿//Exercícios Para "Estrutura Escolha-Caso"
+
+int dia = 1;
+
+
+Console.WriteLine("Digite o numero corresponde ao mes");
+dia = Convert.ToInt16(Console.ReadLine());
+
+switch (dia)
+{
+
+    case 1:
+        Console.WriteLine("Domingo");
+        break;
+
+    case 2:
+        Console.WriteLine("Segunda");
+        break;
+
+    case 3:
+        Console.WriteLine("Terça");
+        break;
+
+    case 4:
+        Console.WriteLine("Quarta");
+        break;
+
+    case 5:
+        Console.WriteLine("Quinta");
+        break;
+
+    case 6:
+        Console.WriteLine("sexta");
+        break;
+
+    case 7:
+        Console.WriteLine("Sabado");
+        break;
+    default:
+        Console.WriteLine("op invalida");
+        break;
+
+}
