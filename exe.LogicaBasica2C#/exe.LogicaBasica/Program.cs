8c66a452280f699791d6b407f096ae75b175eae3
DAVID CARLOS DOS SANTOS DE JESUS <david.csjesus@senacsp.edu.br>
2025-10-14 20:00:15 -0300

    exe.LogicaBasica2C#

---
+++ exe.LogicaBasica2C#/exe.LogicaBasica/Program.cs
@@ -0,0 +1,17 @@
+﻿//Exercícios de Lógica Básica
+//Escreva um algoritmo que leia dois numeros inteiros e mostre a soma deles
+
+using System;
+
+int nm1;
+int nm2;
+int soma;
+
+Console.WriteLine("Digite o primeiro numero");
+nm1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero");
+nm2 = Convert.ToInt16(Console.ReadLine());
+
+soma = nm1 + nm2;
+Console.WriteLine($"O resultado dos números é {soma}");
